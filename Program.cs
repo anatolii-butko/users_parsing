084f392449a54536e1509175d2e574be2899a9c0
Anatolii Butko <anatolii.butko93@gmail.com>
2022-01-12 10:41:13 +0200

    Some description.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace users_parsing
+{
+
+
+    class Program
+    {
+        class User
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Company { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            const string Filename = @"C:\Users\anatolii.butko\source\repos\users_parsing\users.xml";
+            xDoc.Load(Filename);
+            List<User> users = new List<User>();
+            XmlElement xRoot = xDoc.DocumentElement;
+            if (xRoot != null)
+            {
+                foreach (XmlElement xnode in xRoot)
+                {
+                    User user = new User();
+                    XmlNode attr = xnode.Attributes.GetNamedItem("name");
+                    user.Name = attr?.Value;
+
+                    foreach (XmlNode childnode in xnode.ChildNodes)
+                    {
+                        if (childnode.Name == "company")
+                            user.Company = childnode.InnerText;
+
+                        if (childnode.Name == "age")
+                            user.Age = int.Parse(childnode.InnerText);
+                    }
+                    users.Add(user);
+                }
+                foreach (User u in users)
+                    Console.WriteLine($"{u.Name} ({u.Company}) - {u.Age}");
+            }
+
+
+            
+        }
+    }
+}
